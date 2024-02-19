5d0220f2cc86556da2301e0b0dbd24b0f0fa85dd
rakesh <rakeshkunagi95@gmail.com>
2024-02-19 09:36:37 +0530

    first commit

---
+++ src/main/java/com/rakesh/sms/repository/StudentRepository.java
@@ -0,0 +1,13 @@
+package com.rakesh.sms.repository;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.rakesh.sms.entity.Student;
+
+
+@Repository
+public interface StudentRepository extends JpaRepository<Student, Long> {
+
+}
